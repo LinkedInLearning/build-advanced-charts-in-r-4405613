df679a8dc2bd382279274c15983185f76cc9ad1e
ritagior <rgiordano@gmx.com>
2023-05-31 13:18:38 +0100

    01_01b.R: beginning file video 01_01

---
+++ 01_01b.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+
+## read the data using read_csv
+
+
+## get column names
+
+
+## get column specification
+
+
+## get the dimension of the data frame in rows x columns
+
+
+## create a summary table
+
+
+
+
+## extract all the country
+
+## select data from continents
