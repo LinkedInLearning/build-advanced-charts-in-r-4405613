b6dad4ac2b4d0ed67964b2c2eea4cf2e1d1cb5ae
ritagior <rgiordano@gmx.com>
2023-06-01 15:27:23 +0100

    add script 02_03b.R and data

---
+++ 02_03b.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(circlize)
+
+## read the matrix
+matr <- read.table("data/matrix.txt") %>%
+  read_csv("data/renewable_data_35.csv",
+           show_col_types = FALSE) %>%
+  column_to_rownames("name") %>%
+  as.matrix()
+
+## Create a very basic chord diagram
+
+## get information about the diagram
+
+## clear the environment
+
+## create another chord diagram at -90 degree and add a gap,
+
+chordDiagram(
+
+)
+
+## change the column's name of the matrix
+
+circos.par(start.degree = )
+chordDiagram(
+  matr,
+)
+
+circos.clear()
