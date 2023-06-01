8e6ed187639ab7677a0cde36a67bd178b024a7ca
ritagior <rgiordano@gmx.com>
2023-06-01 13:52:58 +0100

    02_01.b.R script

---
+++ 02_01b.R
@@ -0,0 +1,12 @@
+## Load circlize
+
+
+## sector names
+sectors <- c("Norway", "New Zealand", "Luxembourg", "Vietnam",
+             "United Kingdom", "Slovenia", "Greece", "Kazakhstan")
+
+
+## initialise layout
+
+
+## plotting region
