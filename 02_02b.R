135531284932c92f46ec1929af431efdfe2b05e6
ritagior <rgiordano@gmx.com>
2023-06-01 14:49:33 +0100

    add script 02_02b.R and data

---
+++ 02_02b.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(circlize)
+
+## read the data from renewable energy
+data <- read_csv("data/renewable_continents.csv")
+
+## I select the variable I will use to create the chord diagram
+## Remove all the data with primary energy share = 0%
+data_0 <- data %>%
+  filter(renewables_perc_electricity > 0,
+         year == 2021)
+
+
+## we run again the summary statistics
+
+
+## we select all the data with share of primary energy > 35,
+## which is the mean in 2021
+
+
+
+## data for the matrix
+
+
