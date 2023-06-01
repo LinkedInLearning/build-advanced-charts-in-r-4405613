520a8692ed23c58032579af64170ae353a9c4dbe
ritagior <rgiordano@gmx.com>
2023-06-01 12:08:21 +0100

    add beginning script: 01_02b.R and data

---
+++ 01_02b.R
@@ -0,0 +1,21 @@
+## Visual data exploration
+library(tidyverse)
+
+
+## read data from continents
+renewable_country <- read_csv("data/renewable_continents.csv",
+                             show_col_types = FALSE)
+
+
+## create histogram
+
+
+## create a frequency polygons
+
+
+## Countries that use more than 90% of renewable energy
+
+
+## Line chart for Europe and countries that use more
+## than 50% of renewable energy
+
