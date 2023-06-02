ca7afd130aef2d2c3b32a0e13171b14ec909b158
ritagior <rgiordano@gmx.com>
2023-06-02 12:12:25 +0100

    remove empty lines

--- 04_01b.R
+++ 04_01b.R
@@ -1,21 +1,18 @@
-## Video 04_01 colours
-
 library(tidyverse)
 library(RColorBrewer)
 library(colorblindr)
-library(viridis)
 library(recolorize)
 
 ## read slope graph with default colours
 
-## create a grid to check if the color palette is coloblind friendly
 
-## use viridis package to add viridis palette
+## change colours of the slope graph
+
+
+## use viridis palette from ggplot2
 
 
 ## check if is colour-blind friendly
-
-#for discrete data
 
 
 ## use R colours brewer to find colour blind palette
@@ -23,9 +20,12 @@ library(recolorize)
 
 ## we select one of this palette and run again the code
 
+
 ## use the package recolorize to change the saturations.
 
+
 ## transform into rgb
+
 
 ## adjust saturation and brightness
 
