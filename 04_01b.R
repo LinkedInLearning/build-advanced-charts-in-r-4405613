1ce33748d07d91333761639d38ebd65bd5150c0c
ritagior <rgiordano@gmx.com>
2023-06-02 12:12:03 +0100

    script and data

---
+++ 04_01b.R
@@ -0,0 +1,37 @@
+## Video 04_01 colours
+
+library(tidyverse)
+library(RColorBrewer)
+library(colorblindr)
+library(viridis)
+library(recolorize)
+
+## read slope graph with default colours
+
+## create a grid to check if the color palette is coloblind friendly
+
+## use viridis package to add viridis palette
+
+
+## check if is colour-blind friendly
+
+#for discrete data
+
+
+## use R colours brewer to find colour blind palette
+
+
+## we select one of this palette and run again the code
+
+## use the package recolorize to change the saturations.
+
+## transform into rgb
+
+## adjust saturation and brightness
+
+## convert to Hex code
+
+## add the new palette to the original charts
+
+## text again
+
