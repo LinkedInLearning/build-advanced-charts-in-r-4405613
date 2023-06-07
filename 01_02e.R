7fffbf958fb07511d88b26b33f90e7af6dddd7f0
ritagior <rgiordano@gmx.com>
2023-06-07 11:31:48 +0100

    update names

--- 01_02e.R
+++ 01_02e.R
@@ -2,28 +2,28 @@
 library(tidyverse)
 
 ## read data from continents
-renewable_country <- read_csv("data/renewable_continents.csv")
+ren <- read_csv("data/renewable_continents.csv")
 
 ## create histogram
-renewable_country %>%
+ren %>%
   filter(year %in% 2000:2021) %>%
   ggplot() +
   geom_histogram(aes(renewables_perc_electricity), binwidth = 1)
 
 ## create a frequency polygons
-renewable_country %>%
+ren %>%
   ggplot() +
   geom_freqpoly(aes(renewables_perc_electricity, colour = Continent))
 
 ## Countries that use more than 90% of renewable energy
-renewable_country %>%
+ren %>%
   filter(renewables_perc_electricity >= 90) %>%
   ggplot(aes(year, renewables_perc_electricity, colour = territory)) +
   geom_line()
 
 ## Line chart for Europe and countries that use more
 ## than 50% of renewable energy
-renewable_country %>%
+ren %>%
   filter(Continent == "Europe",
          renewables_perc_electricity >= 50) %>%
   ggplot(aes(year, renewables_perc_electricity, colour = territory)) +
