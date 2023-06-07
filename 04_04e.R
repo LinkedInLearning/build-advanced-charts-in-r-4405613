43d53e8bd4037230a2f2baa91a6e38b68d08a926
ritagior <rgiordano@gmx.com>
2023-06-07 12:08:45 +0100

    end script and data

---
+++ 04_04e.R
@@ -0,0 +1,93 @@
+library(tidyverse)
+library(cowplot)
+library(circlize)
+source("R/circos_function.R")
+source("R/04_03/col_grid.R")
+
+## read the matrix
+matr <- read_csv("R/04_02/renewable_data_65.csv",
+                 show_col_types = FALSE) %>%
+  column_to_rownames("name") %>%
+  as.matrix()
+
+## define colour grid for the chord diagram
+col_grid <- col_grid()
+circos.clear()
+## chord diagram
+circos.par(start.degree = -90)
+## formula format for circlize charts
+cd <- ~circos_function(matr, col_grid, small_gap = 4)
+
+circos.clear()
+
+## Read sparklines
+sp <- readRDS("R/04_02/sparkline.rdata")
+
+plot_col <- plot_grid(sp, cd,
+                      labels = c("Share of renewable energy ",
+                                 "Renewable source of energy in 2021"),
+                      ncol = 2,
+                      hjust = 0.1,
+                      label_x = 0.1,
+                      label_y = 1.5,
+                      rel_widths = c(1.1, 2),
+                      rel_heights = c(0.5, 2)) +
+  theme(plot.margin = margin(20, 3, 4, 8))
+
+circos.clear()
+
+## Add a title and subtitle. We need to create a ggplot element
+title_gg <- ggplot() +
+  labs(title = "Use of renewable energies per country",
+       subtitle = "Share of renewable energy greater than 65% in 2021") +
+  theme(
+    plot.title = element_text(size = 24,
+                              hjust = 0.5,
+                              face = "bold"),
+    plot.subtitle = element_text(size = 16,
+                                 hjust = 0.5),
+    plot.margin = margin(18, 0, 12, 0),
+    plot.background = element_rect(
+      fill = "grey92", colour = NA)
+  )
+
+
+panel <- plot_grid(title_gg,
+                   plot_col,
+                   ncol = 1, rel_heights = c(0.1, 1),
+                   align = "v") +
+  theme(
+    plot.background = element_rect(
+      fill = "grey92",
+      colour = NA)
+  )
+panel
+circos.clear()
+# footnote <- ggplot() +
+#   labs(
+#     caption = "Source: Our world in data"
+#   ) +
+#   theme(
+#     plot.caption = element_text(size = 10, hjust = -1.2)
+#   )
+# plot_grid(panel, footnote,
+#           ncol = 1, rel_heights = c(0.1, 1),
+#           align = "v")
+# create footnote
+panel_footnote <- add_sub(panel,
+                        label = "Source: Our world in data",
+                        fontface = "bold",
+                        size = 10,
+                        hjust = -1.2)
+
+txt <- "'other' in the chord diagram refers to\n renewable sources including:\n geothermal, biomass, waste, wave and tidal."
+ggdraw(panel_footnote) +
+  geom_text(
+    aes(x = 0.55, y = 0.2, label = txt),
+    hjust = 0.5, vjust = 0.5,
+    #angle = 0,
+    size = 12/.pt,
+    #color = "black",
+    #inherit.aes = FALSE
+   )
+
