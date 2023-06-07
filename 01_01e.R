cb40191e5bfdc4763a5c8b92e5f6ba22d6569d90
ritagior <rgiordano@gmx.com>
2023-06-07 10:39:06 +0100

    update script

--- 01_01e.R
+++ 01_01e.R
@@ -10,15 +10,8 @@ names(ren)
 ## get column specification
 spec(ren)
 
-## get the dimension of the data frame in rows x columns
-dim(ren)
 
 ## create a summary table
 summary(ren[1:9])
 
-
-## extract all the country
-unique_country <- ren %>%
-  filter(type == "country") %>%
-  distinct(territory)
 
