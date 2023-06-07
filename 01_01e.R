e66d7da0036e8ab5cbf5725e89e573cba405147c
ritagior <rgiordano@gmx.com>
2023-06-07 10:39:42 +0100

    update script

--- 01_01e.R
+++ 01_01e.R
@@ -2,14 +2,13 @@ library(tidyverse)
 
 ## read the data using read_csv
 ren <- read_csv("data/renewable.csv",
-                show_col_types = FALSE) #, col_types = "c")
+                show_col_types = FALSE)
 
 ## get column names
 names(ren)
 
 ## get column specification
 spec(ren)
-
 
 ## create a summary table
 summary(ren[1:9])
