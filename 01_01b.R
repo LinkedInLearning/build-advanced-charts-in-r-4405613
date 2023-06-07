5b116246ff16c4955dea9352070f91168fa496bd
ritagior <rgiordano@gmx.com>
2023-06-07 10:38:15 +0100

    update script

--- 01_01b.R
+++ 01_01b.R
@@ -1,15 +1,12 @@
 library(tidyverse)
 
 ## read the data using read_csv
-ren <- read_csv("data/renewable.csv")
+
 
 ## get column names
 
 
 ## get column specification
-
-
-## get the dimension of the data frame in rows x columns
 
 
 ## create a summary table
