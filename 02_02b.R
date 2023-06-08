eed5d60bf9e003bcafe127b1a42894d82cf90ca5
ritagior <rgiordano@gmx.com>
2023-06-08 14:41:40 +0100

    update script and data

--- 02_02b.R
+++ 02_02b.R
@@ -7,11 +7,10 @@ data <- read_csv("data/renewable_continents.csv",
 ## I select the variable I will use to create the chord diagram
 ## Remove all the data with primary energy share = 0%
 data_0 <- data %>%
-  filter(renewables_perc_electricity > 0,
+  filter(share_electricity > 0,
          year == 2021)
 
-
-## we run again the summary statistics
+## summary statistics
 
 
 ## we select all the data with share of primary energy > 35,
