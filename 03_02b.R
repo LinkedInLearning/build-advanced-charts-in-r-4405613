57bec78e2d0dbbd9b9966581beb69e7934931507
ritagior <rgiordano@gmx.com>
2023-06-13 10:58:04 +0100

    update data and script

--- 03_02b.R
+++ 03_02b.R
@@ -1,16 +1,16 @@
 library(tidyverse)
 
-data <- read_csv("data/renewable_65_perc.csv")
+data <- read_csv("data/renewable_65_perc.csv",
+                 show_col_types = FALSE)
 
 ## I want to compare the share of renewable energy per countries.
 ## We calculate the max and min for each countries
 
 
-
-
-## coliurs for minimum and maximum
+## colours for minimum and maximum
 col <- c("min" = "#2c7bb6", "max" = "#d7191c")
 
 ## create the sparkline
 data %>%
   ggplot(aes(x = , y = ))
+  
