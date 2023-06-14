5d5ad90852278570fd425826966aafa998144459
ritagior <rgiordano@gmx.com>
2023-06-14 11:52:54 +0100

    update script

--- 04_01b.R
+++ 04_01b.R
@@ -4,34 +4,16 @@ library(colorblindr)
 library(recolorize)
 
 ## read slope graph with default colours
-
-
-## change colours of the slope graph
+slope_graph <- readRDS("data/slope_04_01.rdata")
 
 
 ## use viridis palette from ggplot2
 
 
-## check if is colour-blind friendly
-
 
 ## use R colours brewer to find colour blind palette
 
 
-## we select one of this palette and run again the code
-
-
-## use the package recolorize to change the saturations.
-
-
-## transform into rgb
-
-
-## adjust saturation and brightness
-
-## convert to Hex code
-
-## add the new palette to the original charts
-
-## text again
+## ------------------Brand colours -----------------------
+brand <- c("red", "darkgreen", "blue", "purple")
 
