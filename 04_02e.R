48a108b8cb3e742a4c26fd17a29438729eff83a6
ritagior <rgiordano@gmx.com>
2023-06-14 13:57:01 +0100

    update script

--- 04_02e.R
+++ 04_02e.R
@@ -25,7 +25,7 @@ circos.clear()
 sp <- readRDS("data/sparkline.rdata")
 ## Add the two charts in one plot
 plot_grid(sp, cd)
-
+circos.clear()
 plot_panel <- plot_grid(sp, cd,
           labels = c("Share of renewable energy",
                      "Renewable source of energy in 2021"),
@@ -37,18 +37,4 @@ plot_panel <- plot_grid(sp, cd,
   theme(plot.margin = margin(20, 3, 4, 8))
 circos.clear()
 
-plot_col <- plot_grid(sp,
-                      cd,
-                      labels = c("Share of renewable energy ",
-                      "Renewable source of energy in 2021"),
-                      ncol = 2,
-                      #label_colour = "white",
-                      #label_size = 14,
-                      hjust = 0.1,
-                      label_x = 0.1,
-                      label_y = 1.5,
-                      #align = 'v',
-                      rel_widths = c(1.1, 2),
-                      rel_heights = c(0.5, 2)
-) +
-  theme(plot.margin = margin(20, 3, 4, 8))
+plot_panel
