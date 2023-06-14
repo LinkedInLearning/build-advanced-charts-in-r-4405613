fa535d6a76f1d6ac2de412cde37bdf94a946dd7b
ritagior <rgiordano@gmx.com>
2023-06-14 14:08:05 +0100

    update scripts

--- 04_02b.R
+++ 04_02b.R
@@ -7,22 +7,12 @@ matr <- read_csv("renewable_data_65.csv",
   column_to_rownames("name") %>%
   as.matrix()
 
-## Create colour palette for the chord diagram
-source_palette <- c("#1B9E77", "#D95F02", "#7570B3", "#A6761D")
-
-## create structure to assign to each renewable source a specific color
-
-source_colour <- structure(source_palette, names = rownames(matr))
-
-country_colour <- structure(rep("#1F78B4",
-                                length(colnames(matr))),
-                            names = colnames(matr))
-## create a colour grid for the chord diagram
-col_grid <- c(source_colour, country_colour)
+## Colour palette for the chord diagram
+source("col_grid.R")
+col_grid <- col_grid()
 
 ## source the circos function
 source("circos_function.R")
-
 ## chord diagram
 
 
