11910fa5c1427642f17a1e0f93d97c6af763c731
ritagior <rgiordano@gmx.com>
2023-06-14 14:38:05 +0100

    update script

--- 04_02b.R
+++ 04_02b.R
@@ -1,21 +1,19 @@
 library(tidyverse)
 library(circlize)
+library(cowplot)
 
 ## read the data 
-matr <- read_csv("renewable_data_65.csv",
+matr <- read_csv("data/renewable_data_65.csv",
                  show_col_types = FALSE) %>%
   column_to_rownames("name") %>%
   as.matrix()
 
 ## Colour palette for the chord diagram
 source("col_grid.R")
-col_grid <- col_grid()
+
 
 ## source the circos function
 source("circos_function.R")
-## chord diagram
-
-
 ## formula format for circlize charts
 
 
