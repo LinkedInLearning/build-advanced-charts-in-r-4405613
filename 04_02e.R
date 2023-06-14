412af0ac5ec1d942fd3c2779e057c317d053420d
ritagior <rgiordano@gmx.com>
2023-06-14 13:49:49 +0100

    update script and add col_grid function

--- 04_02e.R
+++ 04_02e.R
@@ -4,37 +4,25 @@ library(circlize)
 
 
 ## read the matrix
-matr <- read_csv("R/04_02/renewable_data_65.csv",
+matr <- read_csv("data/renewable_data_65.csv",
                  show_col_types = FALSE) %>%
   column_to_rownames("name") %>%
   as.matrix()
 
-## Create colour palette for the chord diagram
-source_palette <- c("#1B9E77", "#D95F02", "#7570B3", "#A6761D")
+## Colour palette for the chord diagram
+source("col_grid.R")
+col_grid <- col_grid()
 
-## create structure to assign to each renewable source a specific color
 
-source_colour <- structure(source_palette, names = rownames(matr2))
-
-country_colour <- structure(rep("#1F78B4",
-                                length(colnames(matr2))),
-                            names = colnames(matr2))
-## create a colour grid for the chord diagram
-col_grid <- c(source_colour, country_colour)
-
-source("R/circos_function.R")
 ## chord diagram
-circos.par(start.degree = -90)
-circos_function(matr, col_grid, small_gap = 4)
-circos.clear()
-
+source("circos_function.R")
 ## formula format for circlize charts
-cd <- ~circos_function(matr, col_grid, small_gap = 4)
+cd <- ~circos_function(matr, col_grid)
 
 circos.clear()
 
 ## Read sparklines
-sp <- readRDS("R/04_02/sparkline.rdata")
+sp <- readRDS("data/sparkline.rdata")
 ## Add the two charts in one plot
 plot_grid(sp, cd)
 
