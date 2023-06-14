a222ec5b192847a53ad918020140ae89530030d1
ritagior <rgiordano@gmx.com>
2023-06-14 13:54:19 +0100

    fix bug

--- circos_function.R
+++ circos_function.R
@@ -20,12 +20,12 @@ circos_function <- function(matrix, col_grid, big_gap = 70, small_gap = 3) {
         CELL_META$xcenter,
         CELL_META$ylim[1],
         CELL_META$sector.index,
-        facing = "clockwise",
+        facing = "clockwise", ## Label facing clockwise
         niceFacing = TRUE, ## Make sure that the label on the left fit human eyes. Anticlockwise
         adj = c(0, 0.5)    ## offset of the text to male sure that the text doesn't overlap with the track.
       )
     },
     bg.border = NA, ## colour for the border of the plotting region
-    circos.clear()
   )
+  circos.clear()
 }
